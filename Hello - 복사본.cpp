4f6d465c857b1cf38821e45dc0e858abfc31a073
kimyipark <kunwo69@gmail.com>
2021-06-26 22:40:53 +0900

    add

---
+++ Hello - 복사본.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+
+class Data
+{
+	int Data = 0;
+
+public:
+	void SetData(int param)
+	{
+		Data = param;
+	}
+};
+int main(void)
+{
+	cout << "Hello world" << endll;
+
+	return 0;
+}
